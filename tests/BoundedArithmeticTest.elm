f9a169746951d8d9d8b3a06ced07bc58773ea1de
Bojan Ristic <bristic@cbancnetwork.com>
2021-02-22 15:33:05 +0100

    testing bounded arithmetics

--- tests/BoundedArithmeticTest.elm
+++ tests/BoundedArithmeticTest.elm
@@ -18,12 +18,27 @@ suite =
             \_ ->
                 Expect.equal (Arithmetic.fromIntBounded (minBound - 1)) (Err "Underflow")
         , describe "Bounded addition"
-            [ test "Add" (\_ -> Expect.equal 1 1)
+            [ test "Add in bound" <|
+                \_ -> Expect.equal (Arithmetic.addBounded 12345 32142) (Ok 44487)
+            , test "Add out of bounds Overflow" <|
+                \_ -> Expect.equal (Arithmetic.addBounded maxBound 1) (Err "Overflow")
+            , test "Add out of bounds Underflow" <|
+                \_ -> Expect.equal (Arithmetic.addBounded minBound -1) (Err "Underflow")
             ]
         , describe "Bounded subtraction"
-            [ test "Subtract" (\_ -> Expect.equal 1 1)
+            [ test "Subtract in bound" <|
+                \_ -> Expect.equal (Arithmetic.subtractBounded 32142 12345) (Ok 19797)
+            , test "Subtract out of bounds Overflow" <|
+                \_ -> Expect.equal (Arithmetic.subtractBounded maxBound -1) (Err "Overflow")
+            , test "Subtract out of bounds Underflow" <|
+                \_ -> Expect.equal (Arithmetic.subtractBounded minBound 1) (Err "Underflow")
             ]
         , describe "Bounded multiplication"
-            [ test "Multiply" (\_ -> Expect.equal 1 1)
+            [ test "Multiply in bound" <|
+                \_ -> Expect.equal (Arithmetic.multiplyBounded 2 5) (Ok 10)
+            , test "Multiply out of bounds Overflow" <|
+                \_ -> Expect.equal (Arithmetic.multiplyBounded (2 ^ 26) (2 ^ 27)) (Err "Overflow")
+            , test "Multiply out of bounds Underflow" <|
+                \_ -> Expect.equal (Arithmetic.multiplyBounded (2 ^ 26) (-2 ^ 27)) (Err "Underflow")
             ]
         ]
